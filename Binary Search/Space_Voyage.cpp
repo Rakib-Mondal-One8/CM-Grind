8d56da98966e0bca57bb1a2f0c230271bab054fb
Rakib Mondal <143431829+Rakib-Mondal-One8@users.noreply.github.com>
2024-12-05 23:01:00 +0530

    Space voyage solved

--- Binary Search/Space_Voyage.cpp
+++ Binary Search/Space_Voyage.cpp
@@ -58,14 +58,6 @@ ll lcm(ll a, ll b) {return (a / __gcd(a, b)) * b;}
 int nXOR(int n) {if (n % 4 == 0)return n; if (n % 4 == 1)return 1; if (n % 4 == 2)return n + 1; return 0;}
 /*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 /*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
-ll check(vector<ll>&a, vector<ll>&b, ll n, ll x) {
-	ll days = 0;
-	loop(i, 0, n - 1) {
-		days += 1;
-		days += ((a[i] * x) / b[i]);
-	}
-	return days;
-}
 void solve()
 {
 	ll n, c;
@@ -74,41 +66,44 @@ void solve()
 	loop(i, 0, n - 1)cin >> a[i] >> b[i];
 
 	auto go1 = [&]() {
-		ll l = 0, r = 1e18;
+		ll l = 1, r = 1e18;
 		ll ans = -1;
 		while (l <= r) {
 			ll mid = l + (r - l) / 2;
-			ll get = check(a, b, n, mid);
-			if (get > c) {
-				r = mid - 1;
+			ll days = 0;
+			bool inf = 0;
+			loop(i, 0, n - 1) {
+				days += 1;
+				days += ((a[i] * mid) / b[i]);
+				if (log10(a[i]) + log10(mid) > 18)inf = 1;
 			}
-			else if (get < c)l = mid + 1;
-			else ans = mid, r = mid - 1;
+			if (days < c && !inf)l = mid + 1;
+			else r = mid - 1;
 		}
-		return ans;
+		return l;
 	};
 	auto go2 = [&]() {
-		ll l = 0, r = 1e18;
+		ll l = 1, r = 1e18;
 		ll ans = -1;
 		while (l <= r) {
 			ll mid = l + (r - l) / 2;
-			ll get = check(a, b, n, mid);
-			if (get > c) {
-				r = mid - 1;
+			ll days = 0;
+			bool inf = 0;
+			loop(i, 0, n - 1) {
+				days += 1;
+				days += ((a[i] * mid) / b[i]);
+				if (log10(a[i]) + log10(mid) > 18)inf = 1;
 			}
-			else if (get < c)l = mid + 1;
-			else ans = mid, l = mid + 1;
+			if (days <= c && !inf)l = mid + 1;
+			else r = mid - 1;
 		}
-		return ans;
+		return l;
 	};
 	ll left = go1();
 	ll right = go2();
-	debug(left);
-	debug(right);
-	if (left != -1 && right != -1)cout << right - left + (left != 0 && right != 0) ? 1 : 0 << nl;
-	else if (left != -1)cout << 1 << nl;
-	else if (right != -1)cout << 1 << nl;
-	else cout << -1 << nl;
+
+	if (right >= 1e18 - 2)cout << -1 << nl;
+	else cout << right - left  << nl;
 }
 int main()
 {
